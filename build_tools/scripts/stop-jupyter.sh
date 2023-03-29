50e721a681653d38abecfb06dcfcef40a5a5136f
kudokai <kudo.kai@gri.jp>
2023-03-29 19:23:10 +0900

    drop buildtools

--- build_tools/scripts/stop-jupyter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-SCRIPT_FILE_PATH=$(realpath $0)
-DOCKER_DIR=$(dirname $(dirname $SCRIPT_FILE_PATH))/docker
-cd $DOCKER_DIR
-
-docker-compose -f poetry-notebook.yml down --rmi all
