b06034c01a1fa388f2df9b7fca6d4ad398a0bd8c
kudo.kai <kudo.kai@gri.jp>
2023-12-26 04:38:36 +0000

    fix docker installing

--- scripts/reconstruct_docker.sh
+++ scripts/reconstruct_docker.sh
@@ -2,6 +2,5 @@ set -eu
 
 ./stop-jupyter.sh
 # yes | sudo docker builder prune
-./save-jupyter.sh
 ./start-jupyter.sh
 sudo docker container list
--- scripts/stop-jupyter.sh
+++ scripts/stop-jupyter.sh
@@ -6,4 +6,4 @@ SCRIPT_FILE_PATH=$(realpath $0)
 DOCKER_DIR=$(dirname $(dirname $SCRIPT_FILE_PATH))/docker
 cd $DOCKER_DIR
 
-docker-compose -f docker-compose.yml down --rmi all
+docker compose -f docker-compose.yml down --rmi all
